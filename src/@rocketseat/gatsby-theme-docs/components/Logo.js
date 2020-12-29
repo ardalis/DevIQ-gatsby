2786e25f6af70fb9d4389487cee8649e97e2efa8
Ilyana Smith <ilyana@kentsmiths.com>
2020-12-29 14:14:53 -0500

    color and sidebar image fixes fixes #5

---
+++ src/@rocketseat/gatsby-theme-docs/components/Logo.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function Logo(props) {
+  return (
+    <img width={180} height={34} viewBox="0 0 180 34" fill="none" src="/banner.png" alt="DevIQ logo" {...props}>
+    </img>
+  );
+}
---
+++ src/@rocketseat/gatsby-theme-docs/styles/theme.js
@@ -0,0 +1,10 @@
+export default {
+    colors: {
+      primary: '#076FCB',
+      background: '#FFFFFF',
+      shape: `#F7F7FB`,
+      title: `#3D3D4D`,
+      text: `#6C6C80`,
+    },
+  };
+  
