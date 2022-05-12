e8f257b1b77ca1d82a99b960e1a8db1aeed0289a
Chensokheng <52232579+Chensokheng@users.noreply.github.com>
2022-05-13 02:14:53 +0700

    enable user close announcement

--- src/@rocketseat/gatsby-theme-docs/components/Announcement/index.js
+++ src/@rocketseat/gatsby-theme-docs/components/Announcement/index.js
@@ -1,21 +1,38 @@
 import React from 'react';
 import { useLocation } from '@reach/router';
 
-import { Container, Space, Link } from './styles';
+import { Container, Space, Link, HideBtn } from './styles';
 import announcementValues from './values';
+import { useState } from 'react';
 
 export default function Announcement() {
 	const location = useLocation();
+	const hideDate = new Date(parseInt(localStorage.getItem('hideDate') ?? 0));
+	const [hide, setHide] = useState(hideDate > new Date());
+
 	const pathname = location.pathname.split('/')[1];
 	const { text, link } =
 		announcementValues[pathname] ?? announcementValues['default'];
+
+	const handleHide = () => {
+		setHide(true);
+		let date = new Date();
+		const next30days = date.setDate(date.getDate() + 30);
+		localStorage.setItem('hideDate', next30days.toString());
+	};
+
 	return (
 		<>
-			<Container>
-				{text}
-				<Link href={link}>Check it out »</Link>
-			</Container>
-			<Space />
+			{!hide && (
+				<>
+					<Container>
+						{text}
+						<Link href={link}>Check it out »</Link>
+						<HideBtn onClick={handleHide}>Hide</HideBtn>
+					</Container>
+					<Space />
+				</>
+			)}
 		</>
 	);
 }
