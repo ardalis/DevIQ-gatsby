847e946d0fbf90d2b334a0170087495a0a3c53b9
Chensokheng <52232579+Chensokheng@users.noreply.github.com>
2022-05-13 01:40:26 +0700

    add announcement component

---
+++ src/@rocketseat/gatsby-theme-docs/components/Announcement/values.js
@@ -0,0 +1,32 @@
+const announcementValues = {
+	default: {
+		link: 'https://deviq.com/',
+		text:
+			'🤖📘🍻 Hooray! After 3 years of work, we\'ve finally released a new ebook on design patterns!',
+	},
+	// practices: {
+	// 	link: 'https://deviq.com/',
+	// 	text: 'principles Announcement',
+	// },
+	// principles: {
+	// 	link: 'https://deviq.com/',
+	// 	text: 'principles Announcement',
+	// },
+	// values: {
+	// 	link: 'https://deviq.com/',
+	// 	text: 'values Announcement',
+	// },
+	// antipatterns: {
+	// 	link: 'https://deviq.com/',
+	// 	text: 'antipatterns Announcement',
+	// },
+	// 'domain-driven-design': {
+	// 	link: 'https://deviq.com/',
+	// 	text: 'domain-driven-design Announcement',
+	// },
+	// 'design-patterns': {
+	// 	link: 'https://deviq.com/',
+	// 	text: 'design-patterns Announcement',
+	// },
+};
+export default announcementValues;
